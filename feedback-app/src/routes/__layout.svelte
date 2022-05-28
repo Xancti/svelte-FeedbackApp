2a533de3706ea38429e5b4113f9c56d6d8e9513a
xancti <aeonar@live.com>
2022-05-29 00:09:21 +0800

    First commit; app is 75% done

---
+++ feedback-app/src/routes/__layout.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet">
+</svelte:head>
+
+<script>
+    import '../lib/global.css'
+</script>
+
+<nav>
+    <a href="/">Home</a>
+    <a href="/About">Index</a>
+    <a href="/settings">Settings</a>
+  </nav>
+  
+  <slot></slot>
+
---
+++ feedback-app/src/routes/index.svelte
@@ -0,0 +1,41 @@
+<script>
+    import FeedbackList from '../lib/FeedbackList.svelte';
+    import FeedbackStats from '../lib/FeedbackStats.svelte';
+    import FeedbackForm from '../lib/FeedbackForm.svelte';
+
+let feedback = [
+        {
+        id: 1,
+        rating: 8,
+        text: 'Super great, had such a fun time!'
+    },
+        {
+        id: 2,
+        rating: 9,
+        text: 'Oh my gosh, I never knew such a place existed! I have to tell my friends now.'
+    },
+        {
+        id: 3,
+        rating: 7,
+        text: 'Great place for supper, waiter was a bit frustrating though.'
+    }
+    ]
+
+$: count = feedback.length
+$: average = feedback.reduce((a, {rating}) => a + rating, 0) / count
+
+const deleteFeedback = (e) => {
+    const itemId = e.detail
+    feedback = feedback.filter((item) => item.id != itemId)
+}
+
+</script>
+
+<main class=container>
+    <FeedbackForm />
+    <FeedbackStats {count} {average} />
+    <FeedbackList {feedback} on:delete-feedback={deleteFeedback} />
+</main>
+
+<style>
+</style>
